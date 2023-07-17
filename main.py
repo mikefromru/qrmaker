47c48351f2e15a1d5d624e9b3acbdaad32cc5d4c
mikefromru <googlockgood@gmail.com>
2023-07-17 14:43:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
+from kivy.uix.filechooser import FileChooser
+
+import logging
+import os
+
+# 3d
+import qrcode
+
+# mine
+import settings
+
+
+class App(MDApp):
+
+    n = NumericProperty(0)
+
+    def build(self):
+        root = Builder.load_file('main.kv')
+        return root
+
+    def on_start(self):
+        logging.info('Start app...')
+        if not os.path.exists('qcodes'):
+            os.mkdir('qcodes')
+
+    def create_qrcode(self):
+        logging.info('click button')
+        self.n += 1
+
+        user_input = self.root.ids.text_field.text
+
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        
+        filename = f'qcodes/{str(self.n)}.png'
+
+        qr.add_data(user_input)
+        qr.make(fit=True)
+        
+        img = qr.make_image(fill_color='black', back_color='white')
+        img.save(filename)
+
+        self.root.ids.result.opacity = 1
+        self.root.ids.buttons.opacity = 1
+        self.root.ids.qrcode_image.source = filename
+
+
+    def download(self):
+        pass
+
+if __name__ == '__main__':
+    App().run()
+
+#self.root.remove_widget(self.root.ids.result)
+#self.root.add_widget(self.root.ids.result)
+#self.root.remove_widget(self.root.ids.my_card)
+
+
---
+++ settings.py
@@ -0,0 +1,7 @@
+from kivy.utils import platform
+from kivy.core.window import Window
+
+if platform == 'linux':
+    Window.size = (420, 820)
+    Window.top = 10
+    Window.left 
